ae0be49d6bbde23d832ee51151f3e022e5ad57da
AEU <aeuplayground@gmail.com>
2026-02-27 23:21:36 +0900

    docs : BoxLabel 스토리북 작성

--- src/share/BoxLabel.stories.tsx
+++ src/share/BoxLabel.stories.tsx
@@ -1,4 +1,13 @@
-export default {};
+import { fn } from "storybook/test";
+import { BoxLabel } from "@share/BoxLabel.tsx";
+
+export default {
+  component: BoxLabel,
+  title: "BoxLabel",
+  tags: ["autodocs"],
+  excludeStories: /.*Data$/,
+  args: { onClick: fn() },
+};
 
 export const Default = {
   args: {},
