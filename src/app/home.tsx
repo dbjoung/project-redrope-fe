ffab7bc91e4fff4802f1c233062710cea11a98cf
AEU <aeuplayground@gmail.com>
2026-03-12 21:49:26 +0900

    remove : Home 컴포넌트 삭제 (이후 다시 추가 예정)

--- src/app/home.tsx
+++
@@ -1,9 +0,0 @@
-import ButtonBig from "@share/ButtonBig.tsx";
-
-export default function Home() {
-  return (
-    <div className="flex flex-row">
-      <ButtonBig iconName={"camera"} content={"로그인"} onClick={() => {}} />
-    </div>
-  );
-}
