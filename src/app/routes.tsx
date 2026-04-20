b676cd968f2b6b6da57633cfe1407470c4b84efa
AEU <aeuplayground@gmail.com>
2026-04-20 23:51:46 +0900

    feat : route 수정

--- src/app/routes.tsx
+++ src/app/routes.tsx
@@ -1,15 +1,34 @@
 import { createBrowserRouter, RouterProvider } from "react-router";
-import LoginPage from "@/app/auth/LoginPage.tsx";
+import LoginPage from "@page/auth/LoginPage.tsx";
 import AuthLayout from "@/app/layouts/AuthLayout.tsx";
+import LootLayout from "@/app/layouts/LootLayout.tsx";
+import SidebarLayout from "@/app/layouts/SidebarLayout.tsx";
+import WorldPage from "@page/world-list/ui/WorldPage.tsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <AuthLayout />,
+    element: <LootLayout />,
     children: [
       {
-        path: "login",
-        element: <LoginPage />,
+        path: "/",
+        element: <AuthLayout />,
+        children: [
+          {
+            path: "/login",
+            element: <LoginPage />,
+          },
+        ],
+      },
+      {
+        path: "/",
+        element: <SidebarLayout />,
+        children: [
+          {
+            path: "/worlds",
+            element: <WorldPage />,
+          },
+        ],
       },
     ],
   },
