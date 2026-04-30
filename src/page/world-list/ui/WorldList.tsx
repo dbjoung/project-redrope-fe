b9ee3c0471b736af0ecc9121c7a779299fa1187f
AEU <aeuplayground@gmail.com>
2026-04-30 23:03:08 +0900

    feat : world 페이지 관련 컴포넌트

---
+++ src/page/world-list/ui/WorldList.tsx
@@ -0,0 +1,3 @@
+export default function WorldList() {
+  return <section></section>;
+}
---
+++ src/page/world-list/ui/WorldPage.tsx
@@ -0,0 +1,9 @@
+import PageTitle from "@widget/layout/ui/PageTitle.tsx";
+
+export default function WorldPage() {
+  return (
+    <PageTitle title={"내 세계"} description={"세계를 만들어 관리해보세요."}>
+      안녕호세용
+    </PageTitle>
+  );
+}
