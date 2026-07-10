4f531599c8414bdb6479b517e7881755125dc429
AEU <aeuplayground@gmail.com>
2026-07-10 23:55:59 +0900

    chore : nest.js 정상작동 테스트

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -3,12 +3,15 @@ import { AppModule } from "./app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  console.log(process.env.API_CORS_ORIGIN ?? "http://localhost:5173");
+
   app.enableCors({
-    origin: process.env.CORS_ORIGIN ?? "http://localhost:5173",
+    origin: process.env.API_CORS_ORIGIN ?? "http://localhost:5173",
     credentials: true,
   });
 
-  const port = Number(process.env.PORT ?? 3000);
+  const port = Number(process.env.API_PORT ?? 3000);
   await app.listen(port);
 }
 
