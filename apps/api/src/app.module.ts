aa77e7de005a61f9e5a3384412387c39d1a22660
AEU <aeuplayground@gmail.com>
2026-08-04 10:15:00 +0900

    feat : 미들웨어 추가

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,26 +1,47 @@
 import { Module } from "@nestjs/common";
+import { ConfigModule, ConfigService } from "@nestjs/config";
+import { TypeOrmModule } from "@nestjs/typeorm";
+
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
-import { TypeOrmModule } from "@nestjs/typeorm";
-import { ConfigModule } from "@nestjs/config";
+import { UserModule } from "./user/user.module";
+import { AuthModule } from "./auth/auth.module";
+import { RedisModule } from "./redis/redis.module";
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      envFilePath: ["./../../.env"],
+      envFilePath: ["../../.env"],
     }),
-    TypeOrmModule.forRoot({
-      type: "mysql",
-      host: process.env.MYSQL_HOST ?? "localhost",
-      port: Number(process.env.MYSQL_PORT) ?? 3307,
-      username: process.env.MYSQL_USER,
-      password: process.env.MYSQL_ROOT_PASSWORD,
-      database: process.env.MYSQL_DATABASE,
 
-      autoLoadEntities: true,
-      synchronize: true,
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+
+      useFactory: (configService: ConfigService) => {
+        const host = configService.get<string>("MYSQL_HOST") ?? "localhost";
+        const port = Number(configService.get<string>("MYSQL_PORT") ?? "3307");
+        const username = configService.get<string>("MYSQL_USER");
+        const password = configService.get<string>("MYSQL_ROOT_PASSWORD");
+        const database = configService.get<string>("MYSQL_DATABASE");
+
+        return {
+          type: "mysql" as const,
+          host,
+          port,
+          username,
+          password,
+          database,
+          autoLoadEntities: true,
+          synchronize: true,
+        };
+      },
     }),
+
+    UserModule,
+    AuthModule,
+    RedisModule,
   ],
   controllers: [AppController],
   providers: [AppService],
--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,8 +1,38 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
+import { ValidationPipe } from "@nestjs/common";
+import { ResponseInterceptor } from "./common/interceptor/ResponseInterceptor";
+import { ExceptionsFilter } from "./common/exception/ExceptionsFilter";
+import * as cookieParser from "cookie-parser";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use((req, res, next) => {
+    console.log(`[REQUEST] ${req.method} ${req.url}`);
+
+    res.on("finish", () => {
+      console.log(`[RESPONSE] ${res.statusCode}`);
+    });
+
+    next();
+  });
+
+  app.use(cookieParser());
+
+  app.useGlobalInterceptors(new ResponseInterceptor());
+  app.useGlobalFilters(new ExceptionsFilter());
+  app.setGlobalPrefix("api/v1");
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+    }),
+  );
 
   app.enableCors({
     origin: process.env.API_CORS_ORIGIN ?? "http://localhost:5173",
