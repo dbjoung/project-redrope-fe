8c3ab2e786048871801eac92f270f726d22bd773
AEU <aeuplayground@gmail.com>
2026-08-17 19:24:09 +0900

    feat : world 관련 엔티티 추가

---
+++ apps/api/src/world/entity/world.entity.ts
@@ -0,0 +1,24 @@
+import { Category } from "@src/category/entity/category.entity";
+import { DeletableEntity } from "@src/common/entity/deleteable.entity";
+import { UserWorld } from "@src/world/entity/user_world.entity";
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class World extends DeletableEntity {
+  @PrimaryGeneratedColumn()
+  id!: number;
+
+  @Column()
+  title!: string;
+
+  @Column()
+  description!: string | null;
+
+  @OneToMany(() => Category, (cate) => cate.parentWorld, {
+    onDelete: "CASCADE",
+  })
+  categories!: Category[];
+
+  @OneToMany(() => UserWorld, (uw) => uw.world)
+  userWorlds!: UserWorld[];
+}
